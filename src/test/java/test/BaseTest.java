870c039df2bb8d5ff5d62ac623b3dc0f090b1fa5
Ivan Surus <ivan_surus@epam.com>
2018-01-08 15:45:09 +0300

    add cross-browser option

--- src/test/java/test/BaseTest.java
+++ src/test/java/test/BaseTest.java
@@ -2,18 +2,27 @@ package test;
 
 import java.net.MalformedURLException;
 import java.util.concurrent.TimeUnit;
-import java.net.URL;  
+import java.net.URL;
 
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
-
+import org.testng.annotations.Parameters;
 
 import component.ToolbarComponent;
 import page.LoginPage;
+
+
+/*The main goal of the grid is to distribute the single test sent to it to a single node
+that matches the requested capabilities.  It will not ask all connected node that can
+execute the test to do so but rather send it to the first it found that can handle
+the test.*/
+
 
 public abstract class BaseTest {
 	
@@ -21,17 +30,31 @@ public abstract class BaseTest {
 	
 	protected WebDriver driver;
 	
-	@BeforeMethod
-	protected void setUp(){
-		ChromeOptions options = new ChromeOptions();
-		try {
-			driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), options);
-		} catch (MalformedURLException e) {
+	@Parameters({"browser", "platform", "version"})
+	@BeforeMethod(alwaysRun=true)
+	protected void setUp(String browser, String platform, String version){
+		try{
+			URL url = new URL("http://localhost:4444/wd/hub");
+		switch (browser){
+		case "chrome":
+			ChromeOptions chromeOptions = new ChromeOptions();
+			chromeOptions.setCapability("platform", Platform.valueOf(platform));
+			chromeOptions.setCapability("version", version);
+		driver = new RemoteWebDriver(url, chromeOptions);
+			break;
+		case "edge":
+			EdgeOptions edgeOptions = new EdgeOptions();
+			edgeOptions.setCapability("platform", Platform.valueOf(platform));
+			edgeOptions.setCapability("version", version);
+		driver = new RemoteWebDriver(url, edgeOptions);
+			break;
+		}
+		}catch (MalformedURLException e) {	
 		}
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(11, TimeUnit.SECONDS);
 	}
-	
+		
 	protected LoginPage doLogIn(){
 		driver.get(URL);
 		return new LoginPage(driver);
